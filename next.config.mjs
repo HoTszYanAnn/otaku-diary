0288e78a455177403820000baf962c7e242b8540
Ann Ho <annho@midland.com.hk>
2024-02-22 20:28:10 +0800

    disable cache on badge list

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  headers: () => [
+    {
+      source: '/badge*',
+      headers: [
+        {
+          key: 'Cache-Control',
+          value: 'no-store',
+        },
+      ],
+    },
+  ],
 };
 
 export default nextConfig;
